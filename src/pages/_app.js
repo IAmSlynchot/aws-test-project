cf68b1ea461c40ab00aab10ea8804c1aa6e2f54e
Sam Stone <slynchot@gmail.com>
2023-10-26 17:47:10 -0400

    adding auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import { Amplify } from 'aws-amplify';
+import config from '../aws-exports';
+Amplify.configure(config);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-export default function Home({ formattedDate }) {
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
-    <>
-      <h1>Static page</h1>
-      <p>This page is static. It was built on {formattedDate}.</p>
-      <p>
-        <a href="/ssr">View a server-side rendered page.</a>
-      </p>
-    </>
+    <View className="App">
+      <Card>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export async function getStaticProps() {
-  const buildDate = Date.now();
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-    timeStyle: "long",
-  }).format(buildDate);
-
-  return { props: { formattedDate } };
-}
+export default withAuthenticator(App);
